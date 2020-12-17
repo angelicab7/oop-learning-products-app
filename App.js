5839840bd15a5dbceaf63f9bfcda6fe3cbe16a14
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-12-17 12:15:51 -0500

    this is going great!

--- App.js
+++ App.js
@@ -7,8 +7,19 @@ class Product{
 }
 
 class UI{
-    addProduct(){
-
+    addProduct(product){
+       const productList = document.getElementById('product-list');
+       const element =  document.createElement('div');
+       element.innerHTML = `
+         <div class="card text-center mb-4">
+         <div class="card-body">
+         <strong>Product name</strong>: ${product.name}
+         <strong>Product price</strong>: ${product.price}
+         <strong>Product year</strong>: ${product.year}
+          </div>
+         </div>
+       `;
+       productList.appendChild(element)
     }
     deleteProduct(){
 
@@ -20,3 +31,14 @@ class UI{
 
 //Dom events
 
+document.getElementById('product-form')
+.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
+    
+    const product = new Product (name, price, year);
+    const ui = new UI();
+    ui.addProduct(product);
+});
