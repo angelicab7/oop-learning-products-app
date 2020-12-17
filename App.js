bd67c0c495f0ac222209a2a05a38a4d7d2633083
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-12-17 13:05:13 -0500

    delete items functionality

--- App.js
+++ App.js
@@ -1,44 +1,57 @@
-class Product{
-    constructor(name, price, year){
-        this.name = name;
-        this.price = price;
-        this.year = year;
-    }
+class Product {
+  constructor(name, price, year) {
+    this.name = name;
+    this.price = price;
+    this.year = year;
+  }
 }
 
-class UI{
-    addProduct(product){
-       const productList = document.getElementById('product-list');
-       const element =  document.createElement('div');
-       element.innerHTML = `
+class UI {
+  addProduct(product) {
+    const productList = document.getElementById("product-list");
+    const element = document.createElement("div");
+    element.innerHTML = `
          <div class="card text-center mb-4">
          <div class="card-body">
-         <strong>Product name</strong>: ${product.name}
-         <strong>Product price</strong>: ${product.price}
-         <strong>Product year</strong>: ${product.year}
+              <strong>Product name</strong>: ${product.name}
+              <strong>Product price</strong>: ${product.price}
+              <strong>Product year</strong>: ${product.year}
+              <a href="#" class="btn btn-info" name="delete">Delete</a>
           </div>
          </div>
        `;
-       productList.appendChild(element)
-    }
-    deleteProduct(){
+    productList.appendChild(element);
+  }
 
-    }
-    showMessage(){
+  resetForm() {
+    document.getElementById("product-form").reset();
+  }
 
-    }
+  deleteProduct(element) {
+      if(element.name === 'delete'){
+        element.parentElement.parentElement.parentElement .remove();
+      }
+
+  }
+  showMessage() {}
 }
 
 //Dom events
 
-document.getElementById('product-form')
-.addEventListener('submit', function(event) {
+document
+  .getElementById("product-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    const name = document.getElementById('name').value;
-    const price = document.getElementById('price').value;
-    const year = document.getElementById('year').value;
-    
-    const product = new Product (name, price, year);
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+    const year = document.getElementById("year").value;
+
+    const product = new Product(name, price, year);
     const ui = new UI();
     ui.addProduct(product);
-});
+    ui.resetForm();
+  });
+  document.getElementById('product-list').addEventListener('click', function(event){
+    const ui = new UI();
+    ui.deleteProduct(event.target);
+  });
